2f39d28d5d2aa8296193b71fe47487bd1593bb08
ababoum <mababou@student.42.fr>
2022-04-27 19:10:09 +0200

    Add cylinder and plan parsing

---
+++ srcs/maths/vectors1.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vectors1.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mababou <mababou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 11:34:28 by mababou           #+#    #+#             */
+/*   Updated: 2022/04/27 18:18:35 by mababou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/miniRT.h"
+
+// constructor
+t_vec	*vector(t_3D_point *p0, t_3D_point *p1)
+{
+	t_vec	*vec;
+	t_data	*data;
+
+	data = get_data(0, 0);
+	vec = malloc_log(data, sizeof(t_vec));
+	vec->x = p1->x - p0->x;
+	vec->y = p1->y - p0->y;
+	vec->z = p1->z - p0->z;
+	return (vec);
+}
+
+// constructor with coordinates
+t_vec	*vector_f(float x, float y, float z)
+{
+	t_vec	*vec;
+	t_data	*data;
+
+	data = get_data(0, 0);
+	vec = malloc_log(data, sizeof(t_vec));
+	vec->x = x;
+	vec->y = y;
+	vec->z = z;
+	return (vec);
+}
+
+float	scalar(t_vec *v1, t_vec *v2)
+{
+	return (v1->x * v2->x + v1->y * v2->y + v3->z * v3->z);
+}
+
+float	norm2(t_vec *v)
+{
+	float	n;
+
+	n = 0;
+	n += pt->x * pt->x;
+	n += pt->y * pt->y;
+	n += pt->z * pt->z;
+
+	return (n);
+}
