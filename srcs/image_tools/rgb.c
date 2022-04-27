99943c84e216bafea437302991bbb6dbdf873fda
plefevre <pierre.lefevre.72@gmail.com>
2022-04-27 15:58:24 +0200

    Camera direction

--- srcs/image_tools/rgb.c
+++ srcs/image_tools/rgb.c
@@ -4,7 +4,7 @@
 
 int arr_toRGB(int *rgb)
 {
-	return rgb[0] + rgb[1] * 256 + rgb[2] * 65536;
+	return rgb[2] + rgb[1] * 256 + rgb[0] * 65536;
 }
 
 
@@ -14,12 +14,12 @@ int rgb_ambiant(int rgb, int *lrgb, float grad)
 	int g;
 	int b;
 
-	r = rgb % 256;
+	r = (rgb / 65536) % 256;
 	g = (rgb / 256) % 256;
-	b = (rgb / 65536) % 256;
+	b = rgb % 256;
 	r *= grad * lrgb[0] / 256;
 	g *= grad * lrgb[1] / 256;
 	b *= grad * lrgb[2] / 256;
 
-	return (r + g * 256 + b * 65536);
+	return (b + g * 256 + r * 65536);
 }
